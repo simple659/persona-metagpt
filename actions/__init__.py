36e7918ef8382adc9e83eb255e4f96c2e4f47fe6
simple659 <xiaowj2002@163.com>
2026-03-04 16:29:21 +0800

    Update __init__.py

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,1 +1,6 @@
 
+from actions.generate_personas import GeneratePersonas
+from actions.validate_prd import ValidatePRDWithPersonas
+from actions.adversarial_review import AdversarialReview
+
+__all__ = ["GeneratePersonas", "ValidatePRDWithPersonas", "AdversarialReview"]
