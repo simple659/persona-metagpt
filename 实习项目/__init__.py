178157fe33a9685410fa352ac5ee6eea883278f8
simple659 <xiaowj2002@163.com>
2026-03-04 15:57:07 +0800

    Delete 实习项目 directory

--- 实习项目/__init__.py
+++
@@ -1,4 +0,0 @@
-from roles.persona_pm import PersonaPM
-from roles.devil_advocate import DevilAdvocate
-
-__all__ = ["PersonaPM", "DevilAdvocate"]
--- 实习项目/mnt/user-data/outputs/persona-metagpt/actions/__init__.py
+++
@@ -1,5 +0,0 @@
-from actions.generate_personas import GeneratePersonas
-from actions.validate_prd import ValidatePRDWithPersonas
-from actions.adversarial_review import AdversarialReview
-
-__all__ = ["GeneratePersonas", "ValidatePRDWithPersonas", "AdversarialReview"]
